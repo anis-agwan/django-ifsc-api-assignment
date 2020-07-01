1a93d8bdccf90f239d0476f5cfc829f09bb5b1b2
anis_agwan <anishagwan@gmail.com>
2020-07-01 18:38:49 +0530

    Admin coded

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Bank, Branch
+from django.contrib import admin
+
+@admin.register(Bank)
+class BankAdmin(admin.ModelAdmin):
+    search_fields = ('bankName',)
+    list_filter = ('bankName', )
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    list_display = (
+        'branchName', 'bank', 'branchCity', 'branchDistrict',
+    )
+    search_fields = ('branchName', 'branchCity')
+    list_filter = ('bank', )
